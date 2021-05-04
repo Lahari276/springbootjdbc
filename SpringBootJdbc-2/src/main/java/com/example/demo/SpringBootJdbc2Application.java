4d80a0b0650ad3436820858e8912a8b125f054a7
Lahari276 <laharireddy147@gmail.com>
2021-05-04 10:34:10 +0530

    jdbc spring boot

---
+++ SpringBootJdbc-2/src/main/java/com/example/demo/SpringBootJdbc2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbc2Application.class, args);
+	}
+
+}
